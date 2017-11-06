1aa3a0c126d5f488db8a4802f5421e012b28e7d2
shivamadeshara <32663087+shivamadeshara@users.noreply.github.com>
2017-11-05 21:20:22 -0800

    Add files via upload

--- src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpModeShivam.java
+++ src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpModeShivam.java
@@ -20,6 +20,8 @@ public class MyFIRSTJavaOpModeShivam extends LinearOpMode {
     private Gyroscope imu;
     private DcMotor motorTest1;
     private DcMotor motorTest2;
+    private DcMotor motorTest3;
+    private DcMotor motorTest4;
     private DigitalChannel digitalTouch;
     private DistanceSensor sensorColorRange;
     private Servo servoTest1;
@@ -29,11 +31,13 @@ public class MyFIRSTJavaOpModeShivam extends LinearOpMode {
     @Override
     public void runOpMode() {
         imu = hardwareMap.get(Gyroscope.class, "imu");
-        motorTest1 = hardwareMap.get(DcMotor.class, "motorTest");
+        motorTest1 = hardwareMap.get(DcMotor.class, "motorTest1");
         motorTest2 = hardwareMap.get(DcMotor.class, "motorTest2");
+        motorTest3 = hardwareMap.get(DcMotor.class, "motorTest3");
+        motorTest4 = hardwareMap.get(DcMotor.class, "motorTest4");
         digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
         sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
-        servoTest1 = hardwareMap.get(Servo.class, "servoTest");
+        servoTest1 = hardwareMap.get(Servo.class, "servoTest1");
         servoTest2 = hardwareMap.get(Servo.class, "servoTest2");
 
         telemetry.addData("Status", "Initialized");
@@ -63,22 +67,32 @@ public class MyFIRSTJavaOpModeShivam extends LinearOpMode {
         } */
 
         // run until the end of the match (driver presses STOP)
-        double tgtPower = 0;
+        double tgtPower1 = 0;
+        double tgtPower2 = 0;;
+        double multiPlier = 0.25;
         while (opModeIsActive()) {
-            tgtPower = -this.gamepad1.left_stick_y;
-            motorTest1.setPower(tgtPower);
-            tgtPower = -this.gamepad1.right_stick_y;
-            motorTest2.setPower(tgtPower);
+            // Gamepad 1
+            tgtPower1 =  - multiPlier * this.gamepad1.left_stick_y;
+            motorTest1.setPower(tgtPower1);
+            tgtPower1 = - multiPlier * this.gamepad1.right_stick_y;
+            motorTest2.setPower(tgtPower1);
+            // GamePad 2
+            // Gamepad 2 is used to control Motor 3 and 4
+            tgtPower2 = - multiPlier * this.gamepad2.left_stick_y;
+            motorTest3.setPower(tgtPower2);
+            tgtPower2 = - multiPlier * this.gamepad2.right_stick_y;
+            motorTest4.setPower(tgtPower2);
             // check to see if we need to move the servo.
-            if(gamepad1.y) {
+            // Gamepad 2 is used to control Servo Motor 1 and 2
+            if(gamepad2.y) {
                 // move to 0 degrees.
                 servoTest1.setPosition(0);
                 servoTest2.setPosition(1);
-            } else if (gamepad1.x || gamepad1.b) {
+            } else if (gamepad2.x || gamepad2.b) {
                 // move to 90 degrees.
                 servoTest1.setPosition(0.5);
                 servoTest2.setPosition(0.5);
-            } else if (gamepad1.a) {
+            } else if (gamepad2.a) {
                // move to 180 degrees.
                 servoTest1.setPosition(1);
                 servoTest2.setPosition(0);
@@ -86,9 +100,12 @@ public class MyFIRSTJavaOpModeShivam extends LinearOpMode {
 
             telemetry.addData("Servo1 Position", servoTest1.getPosition());
             telemetry.addData("Servo2 Position", servoTest2.getPosition());
-            telemetry.addData("Target Power", tgtPower);
+            telemetry.addData("Target Power1", tgtPower1);
+            telemetry.addData("Target Power2", tgtPower2);
             telemetry.addData("Motor1 Power", motorTest1.getPower());
             telemetry.addData("Motor2 Power", motorTest2.getPower());
+            telemetry.addData("Motor3 Power", motorTest3.getPower());
+            telemetry.addData("Motor4 Power", motorTest4.getPower());
             // Following line is for Touch Sensor
             telemetry.addData("Distance (cm)", sensorColorRange.getDistance(DistanceUnit.CM));
             telemetry.addData("Status", "Running");
